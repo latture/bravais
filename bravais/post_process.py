7f8603c1d2f3df37d924dcc20727695bca75a2fc
Ryan Latture <ryan.latture@gmail.com>
2015-06-25 12:38:21 -0700

    Update __all__ and add function to plot data.

--- bravais/post_process.py
+++ bravais/post_process.py
@@ -1,9 +1,25 @@
 __author__ = 'ryan'
 
-__all__ = []
+__all__ = ["categorize_files", "NodalData", "process_data"]
 
 import numpy as np
 from bcs import get_face_nodes
+import matplotlib.pyplot as plt
+import itertools
+
+
+COLORS = {
+        "red"    : (0.7176, 0.1098, 0.1098),
+        "green"  : (0.65 * 0.298, 0.65 * 0.6863, 0.65 * 0.3137),
+        "blue"   : (0.9 * 0.0824, 0.9 * 0.3961, 0.9 * 0.7529),
+        "orange" : (0.85 * 1.0, 0.85 * 0.5961, 0.0),
+        "purple" : (0.49412, 0.3412, 0.7608),
+        "grey"   : (0.45, 0.45, 0.45),
+        "cyan"   : (0.0, 0.7373, 0.8314),
+        "teal"   : (0.0, 0.5882, 0.5333),
+        "lime"   : (0.8039, 0.8627, 0.2235),
+        "brown"  : (0.4745, 0.3333, 0.2824)
+        }
 
 
 class NodalData(object):
@@ -22,6 +38,68 @@ class NodalData(object):
                 self.shear = np.loadtxt(path_to_files + f)
             elif 'bulk' in f:
                 self.bulk = np.loadtxt(path_to_files + f)
+
+
+def plot_data(x, y, labels, xlabel=None, ylabel=None, xlim=None, ylim=None, xticks=None, yticks=None,
+              add_legend=False, savefig=False, showfig=True, filename="test.png"):
+    """
+    Plots the rows of `y` with respect to the vector `x`.
+    :param x: Array. x-axis values.
+    :param y: Matrix. Each row of `y` will constitute a line plotted with respect to `x`. The number of entries in x
+                      must match the number of columns of `y`.
+    :param labels: Array, dtype=`String`. Labels for each line. every row in `y` must have a corresponding label.
+    :param xlabel: `String`. Label for x-axis.
+    :param ylabel: `String`. Label for y-axis.
+    :param xlim: Array, `len(xlim)==2`. Upper and lower limits for the x-axis.
+    :param ylim: Array, `len(ylim)==2`. Upper and lower limits for the y-axis.
+    :param xticks: Array. List of ticks to use on the x-axis. Should be within the upper and lower bounds of the x-axis.
+    :param yticks: Array. List of ticks to use on the y-axis. Should be within the upper and lower bounds of the y-axis.
+    :param add_legend: `Bool`, default=`False`. If `True` a legend will be added to the plot.
+    :param savefig: `Bool`, default=`False`. Whether to save the figure.
+    :param showfig: `Bool`, default=`True`. Whether to show the figure.
+    :param filename: `String`, default=`test.png`. Name of file to save the figure to if `savefig=True`.
+    """
+    assert y.shape[0] == len(labels), "Length of labels does not match the number of rows in y."
+    # define the colors and markers for the plot
+    color = itertools.cycle((COLORS["blue"],
+                             COLORS["green"],
+                             COLORS["red"],
+                             COLORS["orange"],
+                             COLORS["purple"],
+                             COLORS["grey"],
+                             COLORS["cyan"],
+                             COLORS["teal"],
+                             COLORS["lime"],
+                             COLORS["brown"]))
+    marker = itertools.cycle((u's', u'>', u'o', u'D', u'p',u'H', u'^', u'v', u'd'))
+
+    fig = plt.figure(figsize=(8, 6), dpi=150)
+    axis = fig.add_subplot(111)
+
+    for i in xrange(y.shape[0]):
+        axis.plot(x, y[i, :], label=labels[i], linewidth=2, marker=marker.next(), color=color.next())
+
+    plt.rcParams.update({'font.size': 22})
+
+    # update plot labels and format based on user input 
+    if xlabel is not None:
+        axis.set_xlabel(xlabel)
+    if ylabel is not None:
+        axis.set_ylabel(ylabel)
+    if xlim is not None:
+        axis.set_xlim(xlim)
+    if ylim is not None:
+        axis.set_ylim(ylim)
+    if xticks is not None:
+        axis.set_xticks(xticks)
+    if yticks is not None:
+        axis.set_yticks(yticks)
+    if add_legend:
+        plt.legend(prop={'size': 12}, loc="upper right")
+    if savefig:
+        plt.savefig(filename, dpi=150, transparent=True, bbox_inches='tight')
+    if showfig:
+        plt.show()
 
 
 def categorize_files(filenames, keys):
