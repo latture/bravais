082c76d2e8763320fb839a4db2a3b9302bef1b76
Ryan Latture <ryan.latture@gmail.com>
2015-07-12 17:28:11 -0700

    Save job data (nodes, elements, node sets, etc.) to subdirectory and share data for different load types between jobs reducing the need to compute job data for multiple load types.

--- bravais/inp_creator.py
+++ bravais/inp_creator.py
@@ -1,11 +1,30 @@
 import numpy as np
 import os
 from assign_major_axes import assign_major_axes
-from bcs import get_corners, get_load_points, get_face_nodes, get_center_planes
+from bcs import get_face_nodes, get_center_planes
 from cpp_utils import *
 from mesher import Job
+from collections import namedtuple
 
-__all__ = ['create_inp']
+__all__ = ['create_inp', 'InpDescription']
+
+
+class InpDescription(namedtuple('InpDescription', ['job_type', 'dimensions', 'num_elems', 'load_type', 'misc'])):
+    """
+    Holds a description of the inp file being created. Used to construct the file name as well as the
+    naming convention for node and element sets.
+    """
+
+    def __new__(cls, job_type, dimensions, num_elems, load_type, misc=''):
+        """
+        :param job_type   : `String`. Label for current job, e.g. `'FCC-SC'`.
+        :param dimensions : `array_like`, dtype=`Int`. Dimensions of the job along x, y, and z directions in terms of the
+                             number of unit cells, e.g. `[1, 1, 1]`.
+        :param num_elems  : `Int`. The number of elements along each strut of the job.
+        :param load_type  : `String`. Type of loading the job will be subjected to, e.g. `'axial'`
+        :param misc       : `String`. Miscellaneous identifiers to append to the end of the file name.
+        """
+        return super(InpDescription, cls).__new__(cls, job_type, dimensions, num_elems, load_type, misc)
 
 
 def get_instance(instName, partName):
@@ -27,20 +46,38 @@ def get_nset(nsetName, instName):
     """
     return ('*NSET', ', NSET=' + nsetName, ', INSTANCE=' + instName, ', unsorted\n')
 
-def write_dict_to_nset(f, dictionary, instance_name, break_signifier='\n'):
-    for key, value in dictionary.iteritems():
-            for entry in get_nset(key, instance_name):
+def write_keys_to_nset(f, save_path, keys, instance_name, break_signifier='\n'):
+    """
+    Writes node sets to the inp file `f` with include directives to `save_path/keys.csv` and node set names
+    designated by keys. All node sets pertain to the instance defined by the variable `instance_name`.
+    :param f: file to the node set data to.
+    :param save_path: `String`. Path to the node csv data.
+    :param keys: `array_like`, dtype=`String`. List of keys specifying node set names.
+    :param instance_name: `String`. Name of the instance associated with the node sets.
+    :param break_signifier: `String`, default=`'\n'`. A break signifier to write between node sets to improve readability.
+    """
+    for k in keys:
+            for entry in get_nset(k, instance_name):
                 f.write(entry)
-            for idx in value:
-                f.write('%d\n' % idx)
+            f.write('*INCLUDE, INPUT=%s\n' % (save_path+k+'.csv'))
             f.write(break_signifier)
 
-def create_inp(file_name, jobs, radii, load_type, strain,
+def write_dict_to_csv(save_path, dictionary):
+    """
+    Writes the key/value pairs in dictionary to csv files at the location specified by `save_path`.
+    :param save_path: `String`. Path to folder where csv files should be saved.
+    :param dictionary: `Dict`. List of key value pairs where keys are strings specifying the name of the node set and
+    values are arrays of values that are in the node set.
+    """
+    for key, value in dictionary.iteritems():
+        np.savetxt(save_path+key+'.csv', value, fmt='%d')
+
+def create_inp(inp_description, jobs, radii, load_type, strain, csv_dir='csv_data',
                youngs_modulus=200.0e9, shear_modulus=80.0e9, poisson_ratio=0.3):
     """
-    Creates an Abaqus input file for the Bravais lattice specified in job with the name `file_name.inp`
+    Creates an Abaqus input file for the Bravais lattices specified in jobs.
 
-    :param file_name         : `String`. The name of the input file.
+    :param inp_description   : `InpDescription`. Description of the input file.
     :param jobs              : `Tuple`, dtype=`BravaisJob`. The mesh to analyze.
                                 Must have member variables nodes and elems which are numpy arrays.
     :param radii             : `Tuple`, dtype=`Float`. The radius of the cross-sectional area of the struts
@@ -55,24 +92,6 @@ def create_inp(file_name, jobs, radii, load_type, strain,
     assert len(jobs) == len(radii), 'Length of input jobs and radii do not match.'
     assert len(jobs) <= 2, 'Too many jobs. Job merging currently only works with 2 elementary meshes.'
 
-    job = jobs[0]
-    elsets = ()
-    major_axes = []
-    unique_major_axes = []
-    if len(jobs) == 1:
-        elsets = (range(job.elems.shape[0]),)
-        major_axes.append(assign_major_axes(job))
-        unique_major_axes.append(np.asarray(delete_duplicates_dbl(major_axes[0])))
-    else:
-        for j in jobs[1:]:
-            elsets = job.merge(j, return_elem_indices=True)
-        for set in elsets:
-            _job_temp = Job(job.nodes, job.elems[np.array(set)])
-            _major_axes_temp = assign_major_axes(_job_temp)
-            _unique_major_axes_temp = np.asarray(delete_duplicates_dbl(_major_axes_temp))
-            major_axes.append(_major_axes_temp)
-            unique_major_axes.append(_unique_major_axes_temp)
-
     material_data = ['*MATERIAL, NAME=mat', '*ELASTIC', '%f, %f' % (youngs_modulus, poisson_ratio)]
     assembly_data_header = ['*ASSEMBLY, NAME=assembly']
     assembly_data_footer = ['*END ASSEMBLY']
@@ -86,57 +105,127 @@ def create_inp(file_name, jobs, radii, load_type, strain,
 
     break_signifier = '********************************************************\n'
 
-    f = open(os.getcwd() + '/' + file_name + '.inp', 'w')
+    dims_str = ''
+    for i in range(len(inp_description.dimensions)):
+        dims_str += '%d' % inp_description.dimensions[i]
+        if i < len(inp_description.dimensions) - 1:
+            dims_str += 'x'
+
+    if csv_dir[-1] != '/':
+        csv_dir += '/'
+
+    save_path = csv_dir + inp_description.job_type + '/' + dims_str + '/'
+
+    job_data_available = False
+    if os.path.exists(save_path):
+        job_data_available = os.path.isfile(save_path+'.data_available')
+    else:
+        os.makedirs(save_path)
+
+    file_name = '%s_%s_%s-elems_%s_%s.inp' % \
+                (inp_description.job_type,
+                 dims_str,
+                 inp_description.num_elems,
+                 inp_description.load_type,
+                 inp_description.misc)
+
+    unique_major_axes_filename = save_path + inp_description.job_type + '_' + dims_str + '_' + 'unique_major_axes'
+
+    if not job_data_available:
+        job = jobs[0]
+        major_axes = []
+        unique_major_axes = []
+
+        if len(jobs) == 1:
+            elsets = (range(job.elems.shape[0]),)
+            major_axes.append(assign_major_axes(job))
+            unique_major_axes.append(np.asarray(delete_duplicates_dbl(major_axes[0])))
+        else:
+            for j in jobs[1:]:
+                elsets = job.merge(j, return_elem_indices=True)
+            for eset in elsets:
+                _job_temp = Job(job.nodes, job.elems[np.asarray(eset)])
+                _major_axes_temp = assign_major_axes(_job_temp)
+                _unique_major_axes_temp = np.asarray(delete_duplicates_dbl(_major_axes_temp))
+                major_axes.append(_major_axes_temp)
+                unique_major_axes.append(_unique_major_axes_temp)
+        for i in range(len(jobs)):
+            np.savetxt(unique_major_axes_filename + '-' + str(i) + '.csv', unique_major_axes[i])
+    else:
+        unique_major_axes = []
+        for i in range(len(jobs)):
+            unique_major_axes.append(np.loadtxt(unique_major_axes_filename + '-' + str(i) + '.csv'))
+        np.asarray(unique_major_axes)
+
+    f = open(os.getcwd() + '/' + file_name, 'w')
+
     f.write('*PREPRINT,MODEL=YES\n')
 
     # [write part
     f.write(part_data_header + part_name + '\n')
     # nodes:
+    nodes_filename = save_path + inp_description.job_type + '_' + dims_str + "-nodes.csv"
     f.write('*NODE, NSET=%s\n' % nodes_name)
-    for i in xrange(job.nodes.shape[0]):
-        f.write('%d, ' % (i + 1))
-        for j in xrange(job.nodes.shape[1]):
-            f.write('%.12f' % job.nodes[i, j])
-            if j != job.nodes.shape[1] - 1:
-                f.write(', ')
-        f.write('\n')
+    f.write('*INCLUDE, INPUT=%s\n' % nodes_filename)
+    if not os.path.isfile(nodes_filename):
+        node_str = ''
+        for i in xrange(job.nodes.shape[0]):
+            node_str += '%d, ' % (i + 1)
+            for j in xrange(job.nodes.shape[1]):
+                node_str += '%.12f' % job.nodes[i, j]
+                if j != job.nodes.shape[1] - 1:
+                    node_str += ', '
+                else:
+                    node_str += '\n'
+        with open(nodes_filename, "w") as csv_file:
+            csv_file.write(node_str)
 
     # elems:
-    for i, elset in enumerate(elsets):
+    for i in range(len(jobs)):
+        elset_filename = save_path + inp_description.job_type + '_' + dims_str + '-' + elems_name + '-' + str(i + 1) + '.csv'
         f.write('*ELEMENT, TYPE=B31, ELSET=%s\n' % (elems_name + '-' + str(i + 1)))
-        for idx in elset:
-            f.write('%d, ' % (idx + 1))
-            for j, node_num in enumerate(job.elems[idx]):
-                f.write('%d' % (node_num + 1))
-                if j != job.elems.shape[1] - 1:
-                    f.write(', ')
-            f.write('\n')
+        f.write('*INCLUDE, INPUT=%s\n' % elset_filename)
+        if not os.path.isfile(elset_filename):
+            elem_str = ''
+            for idx in elsets[i]:
+                elem_str += '%d, ' % (idx + 1)
+                for j, node_num in enumerate(job.elems[idx]):
+                    elem_str += '%d' % (node_num + 1)
+                    if j != job.elems.shape[1] - 1:
+                        elem_str += ', '
+                elem_str += '\n'
+            with open(elset_filename, 'w') as csv_file:
+                csv_file.write(elem_str)
 
     # sections:
-    for i in xrange(len(elsets)):
+    for i in xrange(len(jobs)):
         uma_rows, uma_cols = unique_major_axes[i].shape
         for j in xrange(uma_rows):
             set_name = elems_name + '_Sect_Group-' + str(i + 1) + '-' + str(j + 1)
+            set_filename = save_path + set_name + '.csv'
             f.write('*ELSET, ELSET=%s\n' % set_name)
-            rows = np.asarray(test_rows(unique_major_axes[i][j], major_axes[i]))
+            f.write('*INCLUDE, INPUT=%s\n' % set_filename)
+            if not os.path.isfile(set_filename):
+                rows = np.asarray(test_rows(unique_major_axes[i][j], major_axes[i]))
 
-            output_elems = ''
-            for idx in rows:
-                output_elems += '%d\n' % (elsets[i][idx] + 1)
-            f.write(output_elems)
+                output_elems = ''
+                for idx in rows:
+                    output_elems += '%d\n' % (elsets[i][idx] + 1)
+                with open(set_filename, 'w') as csv_file:
+                    csv_file.write(output_elems)
 
-            norm_vec = ''
-            for k in xrange(uma_cols):
-                norm_vec += str(unique_major_axes[i][j, k])
-                if k != uma_cols - 1:
-                    norm_vec += ', '
+                norm_vec = ''
+                for k in xrange(uma_cols):
+                    norm_vec += str(unique_major_axes[i][j, k])
+                    if k != uma_cols - 1:
+                        norm_vec += ', '
 
-            sect_data = ['*BEAM GENERAL SECTION, SECTION=CIRC, ELSET=' + set_name,
-                          str(radii[i]), norm_vec, '%f, %f' % (youngs_modulus, shear_modulus)]
+                sect_data = ['*BEAM GENERAL SECTION, SECTION=CIRC, ELSET=' + set_name,
+                             str(radii[i]), norm_vec, '%f, %f' % (youngs_modulus, shear_modulus)]
 
-            # write the section data
-            for line in sect_data:
-                f.write('%s\n' % line)
+                # write the section data
+                for line in sect_data:
+                    f.write('%s\n' % line)
 
     f.write(part_data_footer + '\n')
     f.write(break_signifier)
@@ -156,32 +245,8 @@ def create_inp(file_name, jobs, radii, load_type, strain,
     for line in get_instance(instance_name, part_name):
         f.write(line)
 
-    # get mesh faces:
-    xmax_nodes, xmin_nodes = get_face_nodes(job.nodes, 0)
-    ymax_nodes, ymin_nodes = get_face_nodes(job.nodes, 1)
-    zmax_nodes, zmin_nodes = get_face_nodes(job.nodes, 2)
-
-    # calculate length along x, y, and z directions
-
-    xlen = np.linalg.norm(job.nodes[xmax_nodes[0]] - job.nodes[xmin_nodes[0]])
-    ylen = np.linalg.norm(job.nodes[ymax_nodes[0]] - job.nodes[ymin_nodes[0]])
-    zlen = np.linalg.norm(job.nodes[zmax_nodes[0]] - job.nodes[zmin_nodes[0]])
-
-    minmax_nodes = {
-        'xmin' : xmin_nodes + 1,
-        'xmax' : xmax_nodes + 1,
-
-        'ymin' : ymin_nodes + 1,
-        'ymax' : ymax_nodes + 1,
-
-        'zmin' : zmin_nodes + 1,
-        'zmax' : zmax_nodes + 1,
-    }
-
-    write_dict_to_nset(f, minmax_nodes, instance_name, break_signifier=break_signifier)
-
-    # find the load points on each face
-    loadpoints = {}
+    centerplane_keys = ['yz_center_plane', 'xy_center_plane']
+    face_node_keys = ['xmax', 'xmin', 'ymax', 'ymin', 'zmax', 'zmin']
 
     if load_type.lower() == 'axial':
         loadpoint_keys = ['xmax', 'xmin']
@@ -192,21 +257,45 @@ def create_inp(file_name, jobs, radii, load_type, strain,
     else:
         raise Exception('Load type: %s is not a valid load type. Please choose from `axial`, `shear`, or `bulk`.'
                         % load_type)
-    
-    for key in loadpoint_keys:
-        loadpoints[key + '_loadpoints'] = minmax_nodes[key]
-    
-    write_dict_to_nset(f, loadpoints, instance_name, break_signifier=break_signifier)
+    write_keys_to_nset(f, save_path, face_node_keys, instance_name, break_signifier=break_signifier)
+    write_keys_to_nset(f, save_path, centerplane_keys, instance_name, break_signifier=break_signifier)
 
-    # find center symmetry planes
-    centerplanes = {}
-    centerplane_keys = ['yz_center_plane', 'xy_center_plane']
-    centerplane_values = get_center_planes(job.nodes)
-    
-    for i in range(len(centerplane_keys)):
-        centerplanes[centerplane_keys[i]] = centerplane_values[i] + 1
-    
-    write_dict_to_nset(f, centerplanes, instance_name, break_signifier=break_signifier)
+    if not job_data_available:
+        # get mesh faces:
+        xmax_nodes, xmin_nodes = get_face_nodes(job.nodes, 0)
+        ymax_nodes, ymin_nodes = get_face_nodes(job.nodes, 1)
+        zmax_nodes, zmin_nodes = get_face_nodes(job.nodes, 2)
+
+        # calculate length along x, y, and z directions
+        xlen = np.linalg.norm(job.nodes[xmax_nodes[0]] - job.nodes[xmin_nodes[0]])
+        ylen = np.linalg.norm(job.nodes[ymax_nodes[0]] - job.nodes[ymin_nodes[0]])
+        zlen = np.linalg.norm(job.nodes[zmax_nodes[0]] - job.nodes[zmin_nodes[0]])
+        lengths = np.array([xlen, ylen, zlen])
+        np.savetxt(save_path+'lengths.csv', lengths)
+
+        minmax_nodes = {
+            'xmin' : xmin_nodes + 1,
+            'xmax' : xmax_nodes + 1,
+
+            'ymin' : ymin_nodes + 1,
+            'ymax' : ymax_nodes + 1,
+
+            'zmin' : zmin_nodes + 1,
+            'zmax' : zmax_nodes + 1,
+        }
+
+        write_dict_to_csv(save_path, minmax_nodes)
+
+        # find center symmetry planes
+        centerplanes = {}
+        centerplane_values = get_center_planes(job.nodes)
+
+        for i in range(len(centerplane_keys)):
+            centerplanes[centerplane_keys[i]] = centerplane_values[i] + 1
+        write_dict_to_csv(save_path, centerplanes)
+
+    else:
+        lengths = np.loadtxt(save_path+'lengths.csv')
 
     # end assembly
     for line in assembly_data_footer:
@@ -227,22 +316,22 @@ def create_inp(file_name, jobs, radii, load_type, strain,
         f.write('%s, 3\n' % centerplane_keys[1])
 
     # apply displacement
-    for key in loadpoints.keys():
+    for key in loadpoint_keys:
         if key[0] == 'x':
             if load_type.lower() != 'shear':
                 direction = 1
             else:
                 direction = 2
-            displacement = strain * xlen / 2.0
+            displacement = strain * lengths[0] / 2.0
         elif key[0] == 'y':
             if load_type.lower() != 'shear':
                 direction = 2
             else:
                 direction = 1
-            displacement = strain * ylen / 2.0
+            displacement = strain * lengths[1] / 2.0
         elif key[0] == 'z':
             direction = 3
-            displacement = strain * zlen / 2.0
+            displacement = strain * lengths[2] / 2.0
         else:
             raise Exception('Direction %s is not valid. Key should specify either x, y, or z direction.' % key[0])
 
@@ -265,3 +354,5 @@ def create_inp(file_name, jobs, radii, load_type, strain,
 
     # close file
     f.close()
+
+    open(save_path+'.data_available', 'a').close()
--- examples/create_and_run.py
+++ examples/create_and_run.py
@@ -4,6 +4,7 @@ import sys
 import os
 import platform
 import time
+import copy
 sys.path.append('../')
 
 if platform.system() == 'Windows':
@@ -11,16 +12,15 @@ if platform.system() == 'Windows':
 else:
     from bravais.unit_cells import SimpleCubic, FCC, BCC, UnitData
     from bravais.mesher import mesh_bravais
-    from bravais.inp_creator import create_inp
+    from bravais.inp_creator import create_inp, InpDescription
     from bravais.python_utils import calc_radius
 
 
 def main():
     if platform.system() != 'Windows':
-        num_elems = 12
+        num_elems = 6
         ar1 = ar2 = 25.0
-        dimx = dimy = dimz = 20
-        dims_str = '_%dx%dx%d_' % (dimx, dimy, dimz)
+        dimx = dimy = dimz = 1
         sc_unit = SimpleCubic(a=1.0, num_elems=num_elems)
         bcc_unit = BCC(a=1.0, num_elems=num_elems)
         fcc_unit = FCC(a=1.0, num_elems=num_elems)
@@ -32,7 +32,7 @@ def main():
             UnitData(unit_cells=(sc_unit, fcc_unit), label='FCC-SC', aspect_ratios=(ar1, ar2)),
             UnitData(unit_cells=(sc_unit, bcc_unit), label='BCC-SC', aspect_ratios=(ar1, ar2)),
         )
-        load_types = ('axial', 'bulk', 'shear')
+        load_types = ('axial', 'bulk', 'shear',)
 
         for data in unit_data:
             jobs = []
@@ -43,22 +43,29 @@ def main():
                 radii.append(calc_radius(unit_cell, num_elems, data.aspect_ratios[i]))
 
             for lt in load_types:
-                filename = data.label + dims_str + lt
-                create_inp(filename, jobs, radii=radii, load_type=lt, strain=0.1)
+                inp_desc = InpDescription(job_type=data.label,
+                                          dimensions=[dimx, dimy, dimz],
+                                          num_elems=num_elems,
+                                          load_type=lt,
+                                          misc=('%.0f-aspect' % ar1))
+                create_inp(inp_desc, copy.deepcopy(jobs), radii=radii, load_type=lt, strain=0.1)
     else:
         all_files = os.listdir(os.getcwd())
 
         # run all .inp files in cwd through Abaqus
         for f in all_files:
-            fbase, ext = f.split('.')
-            if ext == 'inp':
-                abaqus.run_job(fbase)
+            try:
+                fbase, ext = f.split('.')
+                if ext == 'inp':
+                    abaqus.run_job(fbase, abaqus_executable='C:/SIMULIA/Abaqus/Commands/abq6143')
 
-                abaqus.run_script('../bravais/extract_nodal_component.py',
-                                  options=(('-o', fbase+'.odb'), ('-c', 'U'), ('-f', fbase+'_displacements.txt')))
+                    abaqus.run_script('../bravais/extract_nodal_component.py', abaqus_executable='C:/SIMULIA/Abaqus/Commands/abq6143',
+                                      options=(('-o', fbase+'.odb'), ('-c', 'U'), ('-f', fbase+'_displacements.txt')))
 
-                abaqus.run_script('../bravais/extract_nodal_component.py',
-                                  options=(('-o', fbase+'.odb'), ('-c', 'RF'), ('-f', fbase+'_forces.txt')))
+                    abaqus.run_script('../bravais/extract_nodal_component.py', abaqus_executable='C:/SIMULIA/Abaqus/Commands/abq6143',
+                                      options=(('-o', fbase+'.odb'), ('-c', 'RF'), ('-f', fbase+'_forces.txt')))
+            except ValueError:
+                pass
 
         all_files = os.listdir(os.getcwd())
 
@@ -74,7 +81,7 @@ def main():
                 pass
 
         # create needed directories
-        data_dir = os.getcwd()+'/data/'
+        data_dir = os.getcwd()+'/fea_data/'
         if not os.path.exists(data_dir):
             os.mkdir(data_dir)
         for label in unit_labels:
@@ -85,7 +92,7 @@ def main():
         all_files = os.listdir(os.getcwd())
         for label in unit_labels:
             for f in all_files:
-                if label+'_' in f:
+                if label+'_' in f and f[-3:] != 'inp':
                     os.rename(os.getcwd()+'/'+f, data_dir+label+'/'+f)
 
 
