b60ab6345ad109901ae7f4bbc74bbb07655f5f0e
Ryan Latture <ryan.latture@gmail.com>
2015-07-16 13:30:30 -0700

    Modified analysis to calculate and plot data based on the relative percent of material a truss occupies in the combined structure.

--- examples/analyze.py
+++ examples/analyze.py
@@ -7,87 +7,93 @@ import copy
 sys.path.append('../')
 from bravais.unit_cells import SimpleCubic, FCC, BCC, UnitData
 from bravais.mesher import mesh_bravais
-from bravais.python_utils import calc_radius, calc_rel_density
-from bravais.post_process import process_data, plot_data
+from bravais.post_process import process_data, plot_data, COLORS
+from bravais.python_utils import add_tie_line
 
 
-def main():
-    num_elems = 12
-    ar1 = ar2 = 25.0
+def analyze():
+    num_elems = 6
     a = 1.0
     E_o = 200.0e9
-    dimx = dimy = dimz = 20
+    dimx = dimy = dimz = 10
+    percents = np.linspace(0.1, 0.9, 9)
+    percents = np.insert(percents, 0, 0.001)
+    percents = np.append(percents, 0.999)
+    relative_density = 0.01
     sc_unit = SimpleCubic(a=a, num_elems=num_elems)
     bcc_unit = BCC(a=a, num_elems=num_elems)
     fcc_unit = FCC(a=a, num_elems=num_elems)
-    dimensions = sc_unit.nodes[0].shape[-1]
 
     unit_data = (
-        UnitData(unit_cells=(sc_unit,), label='SimpleCubic', aspect_ratios=(ar1,)),
-        UnitData(unit_cells=(bcc_unit,), label='BCC', aspect_ratios=(ar1,)),
-        UnitData(unit_cells=(fcc_unit,), label='FCC', aspect_ratios=(ar1,)),
-        UnitData(unit_cells=(sc_unit, bcc_unit), label='SC-BCC', aspect_ratios=(ar1, ar2)),
-        UnitData(unit_cells=(sc_unit, fcc_unit), label='SC-FCC', aspect_ratios=(ar1, ar2))
+        UnitData(unit_cells=(sc_unit, bcc_unit), label='BCC-SC'),
+        UnitData(unit_cells=(sc_unit, fcc_unit), label='FCC-SC'),
     )
 
-    E = np.empty((len(unit_data), dimensions))
-    G = np.empty_like(E)
-    v = np.empty_like(E)
-    K = np.empty_like(E)
+    unit_colors = {
+        'BCC-SC' : (COLORS["orange"], COLORS["black"]),
+        'FCC-SC' : (COLORS["purple"], COLORS["black"])
+    }
 
-    data_dir = os.getcwd() + '/data/'
+    data_dir = '/home/ryan/Desktop/bravais-cnsi/examples/fea_data/'
 
     for data in unit_data:
-        jobs = []
-        radii = []
-        areas = []
-        for i, unit_cell in enumerate(data.unit_cells):
-            j = mesh_bravais(unit_cell, dimx, dimy, dimz)
-            jobs.append(j)
-            radius = calc_radius(unit_cell, num_elems, data.aspect_ratios[i])
-            radii.append(radius)
-            areas.append(np.pi * radius**2)
+        E = np.empty(len(percents))
+        G = np.empty_like(E)
+        v = np.empty_like(E)
+        K = np.empty_like(E)
 
+        job = mesh_bravais(data.unit_cells[0], dimx, dimy, dimz)
         if len(data.unit_cells) > 1:
-            j = copy.deepcopy(jobs[0])
-            for k in range(1, len(jobs)):
-                j.merge(jobs[k])
+            for i in range(1, len(data.unit_cells)):
+                job.merge(mesh_bravais(data.unit_cells[i], dimx, dimy, dimz))
 
-        current_dir = data_dir + data.label + "/"
-        current_files = os.listdir(current_dir)
-        txt_files = []
-        for f in current_files:
-            try:
-                fbase, ext = f.split('.')
-                if ext == 'txt':
-                    txt_files.append(f)
-            except ValueError:
-                pass
+        for i, p in enumerate(percents):
+            percent_str = ('%0.f' % (p * 100)).zfill(3) + '-percent'
+            current_dir = data_dir + data.label + "/"
+            current_files = os.listdir(current_dir)
+            txt_files = []
+            for f in current_files:
+                try:
+                    fbase, ext = f.split('.')
+                    if ext == 'txt' and percent_str in fbase:
+                        txt_files.append(f)
+                except ValueError:
+                    pass
 
-        print "Processing %s..." % data.label
-        E_i, v_i, G_i, K_i = process_data(j, current_files, path_to_files=current_dir)
-        total_volume = a**3
-        relative_density = calc_rel_density(total_volume, jobs, areas)
-        E[i, j] = E_i / (relative_density * E_o)
-        v[i, j] = v_i
-        G[i, j] = G_i / (relative_density * E_o)
-        K[i, j] = K_i / (relative_density * E_o)
+            print "Processing %s %s..." % (data.label, percent_str)
+            E_i, v_i, G_i, K_i = process_data(job, txt_files, path_to_files=current_dir)
+            E[i] = E_i / (relative_density * E_o)
+            v[i] = v_i
+            G[i] = G_i / (relative_density * E_o)
+            K[i] = K_i / (relative_density * E_o)
 
-    unit_cell_names = []
-    for data in unit_data:
-        unit_cell_names.append(data.label)
+        E = add_tie_line(percents, E)
+        G = add_tie_line(percents, G)
+        v = add_tie_line(percents, v)
+        K = add_tie_line(percents, K)
 
-    plot_data(dimensions, E, unit_cell_names, xlabel="Number, n", ylabel="Young's Modulus, $E_{1}/\\rho E_{o}$",
-                           xlim=[0, 27], ylim=[-0.01, 0.35])
-    plot_data(dimensions, v, unit_cell_names, xlabel="Number, n", ylabel="Poission ratio, $\\nu_{12}$",
-                           xlim=[0, 27], ylim=[-0.01, 0.51])
-    plot_data(dimensions, G, unit_cell_names, xlabel="Number, n", ylabel="Shear Modulus, $G_{12}/\\rho E_{o}$",
-                           xlim=[0, 27], ylim=[-0.01, 0.35])
-    plot_data(dimensions, K, unit_cell_names, xlabel="Number, n", ylabel="Bulk modulus, $K/\\rho E_{o}$",
-                           xlim=[0, 27], ylim=[-0.01, 0.35])
+        unit_cell_names = [data.label, data.label + "_tie_line"]
+
+        plot_data(percents, E, unit_cell_names, xlabel="Percent SC, %", 
+                  markers=[u'o', u''], linestyles=['-', '-.'], colors=unit_colors[data.label],
+                  ylabel="Young's Modulus, $E_{1}/\\rho E_{o}$", xlim=[0, 1], ylim=[-0.01, 0.35])
+
+        plot_data(percents, v, unit_cell_names, xlabel="Percent SC, %", 
+                  markers=[u'o', u''], linestyles=['-', '-.'], colors=unit_colors[data.label],
+                  ylabel="Poission ratio, $\\nu_{12}$", xlim=[0, 1], ylim=[-0.01, 0.51])
+
+        plot_data(percents, G, unit_cell_names, xlabel="Percent SC, %",
+                  markers=[u'o', u''], linestyles=['-', '-.'], colors=unit_colors[data.label],
+                  ylabel="Shear Modulus, $G_{12}/\\rho E_{o}$", xlim=[0, 1], ylim=[-0.01, 0.35])
+
+        plot_data(percents, K, unit_cell_names, xlabel="Percent SC, %", 
+                  markers=[u'o', u''], linestyles=['-', '-.'], colors=unit_colors[data.label],
+                  ylabel="Bulk modulus, $K/\\rho E_{o}$", xlim=[0, 1], ylim=[-0.01, 0.35])
+                               
 
 if __name__ == '__main__':
     t1 = time.time()
-    main()
+    analyze()
     t2 = time.time()
     print "Program completed in %.3f seconds" % (t2 - t1)
+
