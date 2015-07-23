b7b61a85037714228771428a2aaa767d478d6fd8
Ryan Latture <ryan.latture@gmail.com>
2015-07-23 10:59:10 -0700

    Reformatted doc strings to align parameter definitions.

--- bravais/python_utils.py
+++ bravais/python_utils.py
@@ -12,7 +12,7 @@ def sort_rows(a):
     Returns input_array with the rows sorted without reordering the entries.
 
     :param a : 2D array (assumed to have 3 columns).
-    :return: 2D array sorted by rows.
+    :return  : 2D array sorted by rows.
     """
 
     return a[np.lexsort((a[:, 2], a[:, 1], a[:, 0]))]
@@ -21,11 +21,12 @@ def sort_rows(a):
 def calc_rel_density(total_volume, jobs, areas):
     """
     Calculates the relative density of one or many jobs.
-    :param total_volume: `Float`. The total volume of all `BravaisJob`'s.
-    :param jobs: `Tuple` dtype=`BravaisJob`. Collection of `BravaisJob`'s contained within the `total_volume`.
-    :param areas: `Tuple` dtype=`Float`. Collection of cross-sectional areas. The `ith` area corresponds to the struts
-                   of the `ith` job.
-    :return: Relative density. `Float`.
+    :param total_volume : `Float`. The total volume of all `BravaisJob`'s.
+    :param jobs         : `Tuple` dtype=`BravaisJob`. Collection of 
+                          `BravaisJob`'s contained within the `total_volume`.
+    :param areas        : `Tuple` dtype=`Float`. Collection of cross-sectional areas. 
+                           The `ith` area corresponds to the struts of the `ith` job.
+    :return             :  Relative density. `Float`.
     """
     assert len(jobs) == len(areas), "jobs and areas are not the same length."
     vol = 0.0
@@ -37,12 +38,12 @@ def calc_rel_density(total_volume, jobs, areas):
 def print_nodes(job1, job2, save_file=False, filename="nodes.csv"):
     """
     Prints the indices where the nodes from `job2` occur in `job1`.
-    :param job1 : `Job`. The Job in which to search for the nodes.
-    :param job2 : `Job`. The Job that contains the nodes to search for.
+    :param job1      : `Job`. The Job in which to search for the nodes.
+    :param job2      : `Job`. The Job that contains the nodes to search for.
     :param save_file : `Bool`. If `True` a file designated by `filename` is saved which contains the indices where
-                       the nodes of `job2` occur in `job1.nodes`.
-    :param filename : `String`. Name of file to save if `save_file` is `True`.
-    :return: Numpy array, dtype=`int`. Indices.
+                        the nodes of `job2` occur in `job1.nodes`.
+    :param filename  : `String`. Name of file to save if `save_file` is `True`.
+    :return          :  Numpy array, dtype=`int`. Indices.
     """
     nodes = np.sort(np.asarray(replace_with_idx(job1.nodes, np.array([job2.nodes]))).ravel())
     if save_file:
@@ -62,10 +63,10 @@ def calc_radius(job, num_elems, aspect_ratio):
     Calculates the strut radius for the elements in `job` for a given aspect ratio
     and number of elements along each strut. Assumes all struts in the job are the
     same length.
-    :param job: Contains `nodes` and `elems` member variables corresponding to the
-    node and element lists for the current mesh.
-    :param num_elems: `Int`. The number of elements along each strut.
-    :param aspect_ratio:  `Float`. Aspect ratio given to each strut.
+    :param job          : Contains `nodes` and `elems` member variables 
+                          corresponding to the node and element lists for the current mesh.
+    :param num_elems    : `Int`. The number of elements along each strut.
+    :param aspect_ratio : `Float`. Aspect ratio given to each strut.
     """
     nn1, nn2 = job.elems[0]
     n1 = job.nodes[nn1]
