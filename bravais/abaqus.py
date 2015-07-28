186ae15e2600cc6172db8999890bdc6572cee93f
Ryan Latture <ryan.latture@gmail.com>
2015-07-27 18:23:37 -0700

    Added ability to run Abaqus using double precision.

--- bravais/abaqus.py
+++ bravais/abaqus.py
@@ -2,15 +2,18 @@ __all__ = ["run_job", "run_script"]
 
 import subprocess
 
-def run_job(inp_file, abaqus_executable='C:/SIMULIA/Abaqus/Commands/abq6135', num_cpus=1):
+def run_job(inp_file, abaqus_executable='C:/SIMULIA/Abaqus/Commands/abq6135', num_cpus=1, double_precision=True):
     """
     Call abaqus via the command line and runs the FE analysis contained in the .inp file.
     :param inp_file: `String`. INP file to analyze, i.e. `path/to/file/filename.inp`.
     :param abaqus_executable: `String`. Path to abaqus executable, i.e. `path/to/abaqus`.
                                         Default = `C:/SIMULIA/Abaqus/Commands/abq6135`
     :param num_cpus: `Int`. Number of cpus to use for analysis. Default = 1.
+    :param double_precision: `Bool`, Default=`True`. Whether to run the analysis using double precision.
     """
     shell_command = '%s interactive job=%s cpus=%d' % (abaqus_executable, inp_file, num_cpus)
+    if double_precision:
+        shell_command += ' double'
     subprocess.call(shell_command, shell=True)
 
 
